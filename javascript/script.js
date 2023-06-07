0bb1df72f1fff29c668fbb35917dc7a6da71f804
MadhuriAdep <madhuriadep26@gmail.com>
2023-06-07 10:06:00 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+// Darkmode
+document.querySelector('.toggle-btn').addEventListener('click', () => {
+    document.body.classList.toggle('darkmode');
+});
+
+// Day counter
+const dayCounter = document.getElementById('counter');
+const start = '2020-04-12';
+const startDate = new Date(start);
+const numFormat = new Intl.NumberFormat("en-US");
+const oneDay = 1000 * 60 * 60 * 24;
+  
+setInterval(() => {
+    const currentDate = new Date();
+    const difference = Math.floor(
+        (currentDate.getTime() - startDate.getTime()) / oneDay );
+        dayCounter.innerText = numFormat.format(difference);
+});
